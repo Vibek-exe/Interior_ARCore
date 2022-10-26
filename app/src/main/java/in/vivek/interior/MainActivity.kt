2e95859b8c7f186baad755fcd79ff1a42c6e7848
Vivek <786vt786@gmail.com>
2022-10-26 19:08:15 +0530

    fiinall

--- app/src/main/java/in/vivek/interior/MainActivity.kt
+++ app/src/main/java/in/vivek/interior/MainActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : ComponentActivity() {
             NotesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
+
+                    //Setting Color
+                    color = MaterialTheme.colors.background ) {
                     Navigation(loginViewModel = loginViewModel)
 
 
